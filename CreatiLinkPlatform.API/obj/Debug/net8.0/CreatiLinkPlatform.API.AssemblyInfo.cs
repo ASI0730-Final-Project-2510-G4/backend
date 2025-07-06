eee31ae704c5cacc6ffd2890b526a3eddff133fd
ElviaRV <elviamarcela14@gmail.com>
2025-07-06 15:56:47 -0500

    feat:  getAllContracts endpoint added

--- CreatiLinkPlatform.API/obj/Debug/net8.0/CreatiLinkPlatform.API.AssemblyInfo.cs
+++ CreatiLinkPlatform.API/obj/Debug/net8.0/CreatiLinkPlatform.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CreatiLinkPlatform.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665baf8b2ee5713bd9db0cc7432349e72de0df16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6a0a9ca60ea0fa4acc94a48eb43c481d2ddc43")]
 [assembly: System.Reflection.AssemblyProductAttribute("CreatiLinkPlatform.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CreatiLinkPlatform.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
